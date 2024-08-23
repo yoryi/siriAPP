2de4d920b8c962cf7ab0c8d8ec29c77b2c861c0a
yoryi <yoryiojeda@outlook.com>
2024-08-23 00:49:39 -0500

    test

--- modules/siri/ios/SiriModule.swift
+++ modules/siri/ios/SiriModule.swift
@@ -1,11 +1,31 @@
+import Foundation
 import ExpoModulesCore
+import Intents
 
+// Define el módulo de Expo
 public class SiriModule: Module {
-  public func definition() -> ModuleDefinition {
-    Name("Siri")
+    public func definition() -> ModuleDefinition {
+        Name("Siri")
 
-    Function("hello") {
-      return "Hello world! 👋"
+        // Define un evento que será enviado a JavaScript
+        Events("onSiriButtonPressed")
+
+        // Función de prueba
+        Function("hello") {
+            return "Hello world! 👋"
+        }
+
+        // Función para empezar a escuchar los eventos de Siri
+        Function("startListeningForSiriEvent") {
+            NotificationCenter.default.addObserver(self, selector: #selector(self.siriButtonPressed), name: NSNotification.Name("SiriButtonPressed"), object: nil)
+        }
     }
-  }
+
+    @objc
+    func siriButtonPressed() {
+        // Envía el evento a JavaScript cuando Siri activa el Intent
+        self.sendEvent("onSiriButtonPressed", [
+            "message": "Siri button pressed!"
+        ])
+    }
 }
