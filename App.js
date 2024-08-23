a070d4767a09dc9890ee8fb792ed4e8426addf36
yoryi <yoryiojeda@outlook.com>
2024-08-23 00:23:36 -0500

    feat: create button

--- App.js
+++ App.js
@@ -1,11 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import * as module from './modules/siri';
 
 export default function App() {
+  const test = () => {
+   const s = module.hello()
+   console.log('res: ', s);
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Siri App!</Text>
       <StatusBar style="auto" />
+      <Text>Siri App!</Text>
+
+      <TouchableOpacity onPress={test} style={{backgroundColor: 'black', padding: 20, borderRadius: 10, marginTop: 30}}>
+        <Text style={{color: 'white'}}>Module</Text>
+      </TouchableOpacity>
+
     </View>
   );
 }
